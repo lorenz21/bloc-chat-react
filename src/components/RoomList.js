dbc09f2140e85f2bccab60a4f615638e9647bae5
Steve <lorenz21@hawaii.edu>
2018-08-30 17:13:51 -1000

    Checkpoint 3 Create Chat Rooms: completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,9 +5,13 @@ class RoomList extends Component {
     super(props);
 
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName: '',
     };
+
     this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.handleUserInput = this.handleUserInput.bind(this);
+    this.createRoom = this.createRoom.bind(this);
   }
 
   componentDidMount() {
@@ -18,15 +22,42 @@ class RoomList extends Component {
     });
   }
 
+  handleUserInput(e){
+    this.setState({
+      newRoomName: e.target.value
+    });
+    console.log(this.state.newRoomName);
+  }
+
+  createRoom() {
+    if(this.state.newRoomName.trim() !== ""){
+      this.roomsRef.push({
+        name: this.state.newRoomName
+      });
+      this.setState({
+        newRoomName: '',
+      });
+    }
+    else{
+      alert("Please enter a Room Name");
+      this.setState({
+        newRoomName: '',
+      });
+    }
+  }
+
   render() {
     return (
       <section>
-      <h1>Bloc Chat</h1>
+      <h1>Bloc Chat Rooms</h1>
       <ul>
       {this.state.rooms.map( room =>
         <li key={room.key}>{room.name}</li>
       )}
       </ul>
+
+      <input type="text" placeholder="Enter new room name..." value={this.state.newRoomName} onChange={this.handleUserInput}/>
+      <button onClick={this.createRoom}>Create New Room</button>
 
       </section>
     );
